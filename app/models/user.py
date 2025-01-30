362c51ef8d6119dcb30b96ff6a5353fbb187b6de
ajf1016 <talropajmal1016@gmail.com>
2025-01-30 12:46:44 +0530

    first commit

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False)  # Either "farmer" or "buyer"
+
+    products = relationship("Product", back_populates="farmer", cascade="all, delete")
+    bids = relationship("Bid", back_populates="buyer", cascade="all, delete")
+    notifications = relationship("Notification", back_populates="user", cascade="all, delete")
